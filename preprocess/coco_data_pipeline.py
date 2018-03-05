45561bc14f4786b9310c84bae9fd6189e53fb243
lzx1413 <lzx1413@live.cn>
2018-03-05 14:07:52 +0800

    add coco mask dataloader

---
+++ preprocess/coco_data_pipeline.py
@@ -0,0 +1,5 @@
+import torch.utils.data as data
+
+class COCOLoader(data.DataLoader):
+    def __init__(self):
+        pass
---
+++ trainval.py
@@ -0,0 +1,154 @@
+# --------------------------------------------------------
+# Pytorch multi-GPU Faster R-CNN
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Jiasen Lu, Jianwei Yang, based on code from Ross Girshick
+# --------------------------------------------------------
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import sys
+import numpy as np
+import argparse
+import pprint
+import pdb
+import time
+
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.optim as optim
+
+import torchvision.transforms as transforms
+from torch.utils.data.sampler import Sampler
+from torch.utils.data import  DataLoader
+from preprocess.data_center import CocoDataset,det_mask_collate,NormalizeImage
+from network.mask_rcnn import MaskRCNN
+
+
+def parse_args():
+  """
+  Parse input arguments
+  """
+  parser = argparse.ArgumentParser(description='Train a Fast R-CNN network')
+  parser.add_argument('--dataset', dest='dataset',
+                      help='training dataset',
+                      default='pascal_voc', type=str)
+  parser.add_argument('--net', dest='net',
+                    help='vgg16, res101',
+                    default='vgg16', type=str)
+  parser.add_argument('--start_epoch', dest='start_epoch',
+                      help='starting epoch',
+                      default=1, type=int)
+  parser.add_argument('--epochs', dest='max_epochs',
+                      help='number of epochs to train',
+                      default=20, type=int)
+  parser.add_argument('--disp_interval', dest='disp_interval',
+                      help='number of iterations to display',
+                      default=100, type=int)
+  parser.add_argument('--checkpoint_interval', dest='checkpoint_interval',
+                      help='number of iterations to display',
+                      default=10000, type=int)
+
+  parser.add_argument('--save_dir', dest='save_dir',
+                      help='directory to save models', default="/srv/share/jyang375/models",
+                      nargs=argparse.REMAINDER)
+  parser.add_argument('--nw', dest='num_workers',
+                      help='number of worker to load data',
+                      default=0, type=int)
+  parser.add_argument('--cuda', dest='cuda',
+                      help='whether use CUDA',
+                      action='store_true')
+  parser.add_argument('--ls', dest='large_scale',
+                      help='whether use large imag scale',
+                      action='store_true')
+  parser.add_argument('--mGPUs', dest='mGPUs',
+                      help='whether use multiple GPUs',
+                      action='store_true')
+  parser.add_argument('--bs', dest='batch_size',
+                      help='batch_size',
+                      default=1, type=int)
+  parser.add_argument('--cag', dest='class_agnostic',
+                      help='whether perform class_agnostic bbox regression',
+                      action='store_true')
+
+# config optimization
+  parser.add_argument('--o', dest='optimizer',
+                      help='training optimizer',
+                      default="sgd", type=str)
+  parser.add_argument('--lr', dest='lr',
+                      help='starting learning rate',
+                      default=0.001, type=float)
+  parser.add_argument('--lr_decay_step', dest='lr_decay_step',
+                      help='step to do learning rate decay, unit is epoch',
+                      default=5, type=int)
+  parser.add_argument('--lr_decay_gamma', dest='lr_decay_gamma',
+                      help='learning rate decay ratio',
+                      default=0.1, type=float)
+# pretrain
+  parser.add_argument('--pretrain',default='',type=str)
+# set training session
+  parser.add_argument('--s', dest='session',
+                      help='training session',
+                      default=1, type=int)
+
+# resume trained model
+  parser.add_argument('--r', dest='resume',
+                      help='resume checkpoint or not',
+                      default=False, type=bool)
+  parser.add_argument('--checksession', dest='checksession',
+                      help='checksession to load model',
+                      default=1, type=int)
+  parser.add_argument('--checkepoch', dest='checkepoch',
+                      help='checkepoch to load model',
+                      default=1, type=int)
+  parser.add_argument('--checkpoint', dest='checkpoint',
+                      help='checkpoint to load model',
+                      default=0, type=int)
+# log and diaplay
+  parser.add_argument('--use_tfboard', dest='use_tfboard',
+                      help='whether use tensorflow tensorboard',
+                      default=False, type=bool)
+
+  args = parser.parse_args()
+  return args
+from config import Config
+
+class CocoTrainConfig(Config):
+    # Give the configuration a recognizable name
+    NAME = "coco_train"
+    # Set batch size to 1 since we'll be running inference on
+    # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 80  # COCO has 80 classes
+
+def to_variable(numpy_data, volatile=False):
+    numpy_data = numpy_data.astype(np.float32)
+    torch_data = torch.from_numpy(numpy_data).float()
+    variable = Variable(torch_data, volatile=volatile)
+    return variable
+if __name__ == '__main__':
+
+  args = parse_args()
+  cfg = CocoTrainConfig()
+  #dataset
+  normalize = NormalizeImage((128.0,128.0,128.0),(1.0,1.0,1.0))
+  coco_dataset = CocoDataset(dataset_dir='/mnt/lvmhdd1/zuoxin/dataset/MSCOCO',subset=['train','minival'],config = cfg,transform=normalize)
+  print('load coco dataset')
+  coco_loader = DataLoader(coco_dataset,batch_size=1,shuffle=True,num_workers=1,collate_fn=det_mask_collate)
+  #model
+  mask_rcnn = MaskRCNN(config=cfg)
+  mask_rcnn = mask_rcnn.cuda()
+  #load pretrain mdoel
+
+  for batch_data in coco_loader:
+      img,image_meta,class_ids,bbox,mask = batch_data
+      img = Variable(img).cuda()
+      y = mask_rcnn.forward(img)
+      loss =
+      print(img.size())
+
+
